d014e3760a7e3ea6b527891082b53c1f42da9e0d
Tauana Pacheco <tauanapachecopp@gmail.com>
2025-01-23 23:19:15 -0300

    feat: add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import './index.css'
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { BrowserRouter, Routes, Route } from "react-router"
-import "./index.css"
 import { MarvelList } from "./pages/Marvel/MarvelList.tsx"
 
 const root = document.getElementById("root") as HTMLElement
